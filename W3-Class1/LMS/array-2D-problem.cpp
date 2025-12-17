accff32b58bb87cd2ec92649d2b0161e9be98778
nop_labs <noppakornwunnoy01@gmail.com>
2025-12-17 11:34:58 +0700

    edit code

--- W3-Class1/LMS/array-2D-problem.cpp
+++ W3-Class1/LMS/array-2D-problem.cpp
@@ -12,13 +12,29 @@ int main(){
             cout << "Exam " << j+1 << " : ";
             cin >> score[i][j];
         }
-        cout << "Average score : " << (score[i][0]+score[i][1]+score[i][2])/3 << endl;
+        //cout << "Average score : " << (score[i][0]+score[i][1]+score[i][2])/3 << endl;
         i++;    cout << "Enter data of next Student (y/n) : ";
         char ch;
         cin >> ch;
+
+        while (ch != 'y' && ch != 'n'){
+            cout << "Enter data of next Student (y/n) : ";
+            cin >> ch;
+        }
+
         if (ch == 'n'){
+            for (int k = 0; k < i; k++){ 
+                float sum = score[k][0] + score[k][1] + score[k][2];
+                float avg = sum / 3.0;
+                cout << "Average score of student " << k+1 << " : " << avg << endl;
+            }
             break;
         }
+
+        if (ch == 'y'){
+            continue;
+        }
+
     }
     return 0;
 }
--- W3-Class1/Learn/array-problem2.cpp
+++ W3-Class1/Learn/array-problem2.cpp
@@ -1,20 +1,7 @@
 /* input score(real number) 3 round No more than 10 people and find mean score each people */
 /* have yes or no to continue in next student */
 /* if finish 10 people break */
-/*
-example use
-Student 1: 
-Exam 1 : 10
-Exam 2 : 20
-Exam 3 : 30
-Average score : 20
-Enter data of next Student (y/n) : y
-Student 2: 
-Exam 1 : 10
-Exam 2 : 20
-Exam 3 : 30
-Enter data of next Student (y/n) : n
-*/
+
 
 #include <iostream>
 using namespace std;
@@ -28,13 +15,29 @@ int main(){
             cout << "Exam " << j+1 << " : ";
             cin >> score[i][j];
         }
-        cout << "Average score : " << (score[i][0]+score[i][1]+score[i][2])/3 << endl;
+        //cout << "Average score : " << (score[i][0]+score[i][1]+score[i][2])/3 << endl;
         i++;    cout << "Enter data of next Student (y/n) : ";
         char ch;
         cin >> ch;
+
+        while (ch != 'y' && ch != 'n'){
+            cout << "Enter data of next Student (y/n) : ";
+            cin >> ch;
+        }
+
         if (ch == 'n'){
+            for (int k = 0; k < i; k++){ 
+                float sum = score[k][0] + score[k][1] + score[k][2];
+                float avg = sum / 3.0;
+                cout << "Average score of student " << k+1 << " : " << avg << endl;
+            }
             break;
         }
+
+        if (ch == 'y'){
+            continue;
+        }
+
     }
     return 0;
 }
